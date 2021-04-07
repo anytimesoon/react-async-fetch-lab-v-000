9e79dc670e3d739fabee5fb785cd2c544b94daac
Damien de Lartigue <damien@driving.vision>
2021-04-07 17:01:51 +0100

    all tests passing

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React from 'react'
+
+export default class App extends React.Component {
+	constructor(){
+		super()
+
+		this.state = {
+			data: ''
+		}
+	}
+
+	render(){
+		return(<div></div>)
+	}
+
+	componentDidMount(){
+		fetch('http://api.open-notify.org/astros.json')
+				.then(response => response.json)
+				.then(json => 
+						this.setState({
+							data: json
+						})
+					)
+	}
+}
